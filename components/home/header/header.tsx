c9e47a22b2be908459c8e3316a6e88cdc32dd14f
PaulNafureanu <paul.nafureanu@gmail.com>
2023-08-11 22:26:26 +0300

    Add fonts and some styles

--- components/home/header/header.tsx
+++ components/home/header/header.tsx
@@ -1,5 +1,20 @@
 import styles from "./header.module.css";
 
 export default function Header() {
-  return <header className={styles.header}>Header</header>;
+  const menus = ["About", "Catalog", "How to use", "Submit", "Contact"];
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <span className={styles.logo}>Logo</span>
+        <ul className={styles.navList}>
+          {menus.map((menu, index) => (
+            <li key={index} className={styles.listItem}>
+              {menu}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- components/home/main/main.tsx
+++ components/home/main/main.tsx
@@ -4,7 +4,12 @@ import SearchBar from "components/home/searchbar/searchbar";
 export default function Main() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Explore Viruses</h1>
+      <div>
+        <h1 className={styles.title}>ExPLORE VIRUSES</h1>
+        <p className={styles.tagline}>
+          Quickly explore viral structures from renewed scientific sources.
+        </p>
+      </div>
       <SearchBar className={styles.searchbar} />
     </main>
   );
